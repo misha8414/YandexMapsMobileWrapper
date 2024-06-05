6934b54618ea7660cab286284848a636b261318f
Vladimir <63112070+PanchenkoVV@users.noreply.github.com>
2024-06-05 12:06:18 +0300

    added platform to package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "YandexMapsMobileWrapper",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "YandexMapsMobileWrapper",
