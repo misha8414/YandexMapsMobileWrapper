d830c2c76714db442e50ab06af646d5bdd4c2e20
Михаил Королев <korolevma96@yandex.ru>
2024-10-21 20:43:15 +0300

    Revert "added platform to package"
    This reverts commit 6934b54618ea7660cab286284848a636b261318f.

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "YandexMapsMobileWrapper",
-    platforms: [
-        .iOS(.v13)
-    ],
     products: [
         .library(
             name: "YandexMapsMobileWrapper",
